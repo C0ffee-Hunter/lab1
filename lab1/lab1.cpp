0eb6d60a6d3091b9f48c946f433ea1ef21a9fd05
C0ffee-Hunter <59738833+C0ffee-Hunter@users.noreply.github.com>
2021-12-07 00:02:16 +0300

    Add files via upload

---
+++ lab1/lab1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "List.h"
+
+using namespace std;
+
+
+int main()
+{
+	List list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.print();
+	cout << list.get_size() << ": size" << endl;
+
+	list.push_front(2384);
+	list.remove(2);
+	cout << "remove #2" << endl;
+	list.print();
+	list.clear();
+	cout << "clear" << endl;
+	list.print();
+
+    return 0;
+}
